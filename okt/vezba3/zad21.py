08f0aba76099dd8f9998d6d11cdbc0cced247d14
Santovac <72747927+Santovac@users.noreply.github.com>
2020-10-26 16:35:24 +0100

    vezbe3, deo 2/2
    uradjen 21. zadatak;
    zadaci 1-4 ostali neuradjeni

--- okt/vezba3/zad21.py
+++ okt/vezba3/zad21.py
@@ -1,1 +1,16 @@
-#WIP
+#A - apsolutna vrednost kvadratnog korena koristeci funkciju sqrt, NJ - vrednost kvadratnog korena dobijena koristeci Njutnovu metodu
+from math import sqrt
+
+def main():
+    x = eval(input("Unesite x:"))
+    n = eval(input("Unesite broj pokusaja:"))
+    NJ=x/2
+    for i in range(1,n+1):
+        NJ=(NJ+x/NJ)/2
+        if(NJ==sqrt(x)):
+            break
+
+    print("Kvadratni koren broja x pomocu funkcije sqrt iznosi:", sqrt(x),", a kvadratni koren dobijen Njutnovom metodom iznosi:", NJ)
+    print("Broj pokusaja: ",i)
+
+main()
